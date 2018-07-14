cb9c575703e0a5665d90f8bc94b3ee008bb59527
Hadrian Paulo Lim <hadrianpaulo@gmail.com>
2018-07-14 11:49:09 +0800

    Update dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  SERVER_URL: '"https://siklab.io/"'
+  SERVER_URL: '"https://policyminer-api.epimetrics.com.ph/"'
 })
