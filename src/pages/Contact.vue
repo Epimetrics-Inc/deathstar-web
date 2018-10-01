3afd1e72f3640022d6e2620c9ac33a7001833d55
hadrianpaulo <hadrianpaulo@gmail.com>
2018-10-01 22:29:59 +0800

    Removed css classes in contact form

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,27 +1,14 @@
 <template>
-  <div id="wrapper" class="about">
-    <div class="landing-content">
-      <div class="col-md-offset-4 col-md-4">
-        <br/>
-        <br/>
+      <div>
         <h3>Comments or Suggestions?</h3>
-        <br/>
         <form name="contact" method="POST" netlify>
-          <div class="form-group">
             <label for="Name">Your Name:</label>
-            <input type="text" name="name" class="form-control" />
-          </div>
-          <div class="form-group">
+            <input type="text" name="name" />
             <label for="email">Your E-Mail:</label>
-            <input type="email" name="email" class="form-control" />
-          </div>
-          <div class="form-group">
+            <input type="email" name="email"/>
             <label for="message">Your Message:</label>
-            <input type="text" name="message" class="form-control" />
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
+            <input type="text" name="message" />
+          <button type="submit">Submit</button>
         </form>
       </div>
-    </div>
-  </div>
 </template>
